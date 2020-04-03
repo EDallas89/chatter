187be7ae7f2b1b29ae21abf27159337cf10cec5f
EDallas89 <inmsanz89@gmail.com>
2020-04-03 09:31:04 +0100

    IMP Removing Stuff

--- __manifest__.py
+++ __manifest__.py
@@ -1,33 +1,16 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Chatter",
-
     'summary': """
         Añade un Chatter al Formulario de los Diarios del Modulo de Facturación""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
     'author': "EDallas",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'website': "https://github.com/EDallas89",
     'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'version': '12.0',
     'depends': ['base', 'account'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
-    # only loaded in demonstration mode
     'installable': True,
     'application': True,
 }
