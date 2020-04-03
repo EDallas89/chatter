8f13818046cfb89800689d33f12cf000f96dfc25
EDallas89 <inmsanz89@gmail.com>
2020-04-03 10:39:28 +0100

    IMP Change View

--- models/models.py
+++ models/models.py
@@ -3,5 +3,5 @@
 from odoo import models, fields, api
 
 class chatter(models.Model):
-     _name = 'account.journal'
-     _inherit = ['account.journal', 'mail.thread', 'mail.activity.mixin']
+     _name = 'account.bank.statement'
+     _inherit = ['account.bank.statement', 'mail.thread', 'mail.activity.mixin']
